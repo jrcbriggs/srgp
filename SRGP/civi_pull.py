a1949474e6e3617be7fff4f73d32d5491e6d35ed
Julian Briggs <Julian.Briggs@sheffield.ac.uk>
2014-12-23 15:25:52 +0000

    civi_pull works: simpler login

--- SRGP/civi_pull.py
+++ SRGP/civi_pull.py
@@ -10,27 +10,18 @@ from time import sleep
 
 class Scrape(object):
 
-    def __init__(self, username, password, url):
-        #         browsername = 'chrome'
-        #         browsername = 'zope.testbrowser'
-        browsername = 'firefox'
-        with Browser(browsername) as b:
+    def __init__(self, url, params):
+        with Browser('firefox') as b:
             b.visit(url)
-            # Find the username form and fill it with the defined username
-            b.find_by_id('edit-name').first.fill(username)
-
-            # Find the password form and fill it with the defined password
-            b.find_by_id('edit-pass').first.fill(password)
+            b.fill_form(params)
             b.find_by_id('edit-submit').first.click()
-            # ,'Search'
-            for label in ('Officers', 'Supporters', 'Volunteers',):
+            for label in ('Officers', 'Supporters', 'Volunteers',): # ,'Search'
                 b.click_link_by_text(label)
                 b.find_by_value('Export to CSV').first.click()
+                sleep(9)
 
 
 if __name__ == '__main__':
-    username = 'j.briggs@phonecoop.coop'
-    password = 'KewGr33n'
-    slug = 'my.greenparty.org.uk/civicrm'
-    url = 'https://{}'.format(slug)  # , password, slug)
-    s = Scrape(username, password, url)
+    params = {'name': 'j.briggs@phonecoop.coop', 'pass': 'KewGr33n', }
+    url = 'https://my.greenparty.org.uk/civicrm'
+    s = Scrape(url, params)
