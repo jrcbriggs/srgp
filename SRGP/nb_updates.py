690e7a46b96a73cc1d4999fc56cac8247c819121
Julian Briggs <j.briggs@phonecoop.coop>
2015-04-15 09:30:53 +0100

    inserted if lines to handle missing fields in nb_updates.py

--- SRGP/nb_updates.py
+++ SRGP/nb_updates.py
@@ -4,6 +4,22 @@
 Created on 4 Feb 2015
 
 @author: julian
+Mods 15-apr-2015 Julian Briggs
+There were 2 problems both involved missing fields.
+
+1. I think NB have added a signup "NationBuilder Support" nb_id=1 which has not tags so referencing tag_list failed for this row.
+I have fixed by inserting addition if at line ~108
+
+        for omit in omits:
+            if omit in fieldmap:        <---new line inserted
+                del fieldmap[omit]
+
+2. I changed to recording "Do not mail" as tag to (more correct) using NB email opt in.
+Surprisingly this seems to have affected the supporter export CSV
+I have fixed by inserting addition if at line ~85
+            for f in self.fieldnames:
+                if f in row_old and f in row_new:        <---new line inserted
+                    if row_old[f] != row_new[f]:
 '''
 from collections import OrderedDict as OD
 from copy import deepcopy
@@ -82,15 +98,16 @@ class NbUpdates(object):
             row_old = self.d0[k]
             row_new = self.d1[k]
             for f in self.fieldnames:
-                if row_old[f] != row_new[f]:
-                    mods.append({
-                        'NB_ID': row_new['nationbuilder_id'],
-                        'CIVICRM_ID': row_new['civicrm_id'],
-                        'FIRST_NAME': row_new['first_name'],
-                        'LAST_NAME': row_new['last_name'],
-                        'FIELDNAME': f,
-                        'OLD_VALUE': row_old[f],
-                        'NEW_VALUE': row_new[f], })
+                if f in row_old and f in row_new:
+                    if row_old[f] != row_new[f]:
+                        mods.append({
+                            'NB_ID': row_new['nationbuilder_id'],
+                            'CIVICRM_ID': row_new['civicrm_id'],
+                            'FIRST_NAME': row_new['first_name'],
+                            'LAST_NAME': row_new['last_name'],
+                            'FIELDNAME': f,
+                            'OLD_VALUE': row_old[f],
+                            'NEW_VALUE': row_new[f], })
         return mods
 
 
@@ -104,7 +121,8 @@ class Main(object):
         omits = 'expires_on,membership_status,membership_type,mobile_number,phone_number,precinct_name,started_at,tag_list'.split(
             ',')
         for omit in omits:
-            del fieldmap[omit]
+            if omit in fieldmap:
+                del fieldmap[omit]
         nu = NbUpdates(ch.t0, ch.t1, nbkey, fieldmap)
         new = nu.new()
         ch.csv_write(new, fn1.replace('.csv', 'NEW.csv'))
