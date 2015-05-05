3eb787440027ae332f9f6474739129e37434badd
Julian Briggs <j.briggs@phonecoop.coop>
2015-05-05 09:07:04 +0100

    updated ghsplit and street split

--- SRGP/gh_split.py
+++ SRGP/gh_split.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
-#Split GH
-#Julian Briggs
-#4-may-2015
-#~ Broom Green
-#~ Broom Walk
-#~ Broomspring Close
-#~ Cavendish Court
-#~ Conway St
-#~ Egerton Close
-#~ Egerton Walk
-#~ Gell St
-#~ Glossop Rd
-#~ Headford Green
-#~ Headford Grove
-#~ Headford Mews
-#~ Victoria St
-#~ Wilkinson St
-#~ Westminster Chambers
+# Split GH
+# Julian Briggs
+# 4-may-2015
+# ~ Broom Green
+# ~ Broom Walk
+# ~ Broomspring Close
+# ~ Cavendish Court
+# ~ Conway St
+# ~ Egerton Close
+# ~ Egerton Walk
+# ~ Gell St
+# ~ Glossop Rd
+# ~ Headford Green
+# ~ Headford Grove
+# ~ Headford Mews
+# ~ Victoria St
+# ~ Wilkinson St
+# ~ Westminster Chambers
 
 from __future__ import print_function
+
 import fileinput
-import sys
 import re
+import sys
 
-regex_jc=',,,(Broom|Cavendish|Conway|Egerton|Gell|Glossop|Headford|Victoria|Wilkinson|Westminster)'
-fn=sys.argv[1]
-fn=fn.replace('.csv','')
 
-first = 1
-fh_jillian = open(fn +'_Jillian.csv', 'w')
-fh_jon    = open(fn +'_Jon.csv', 'w')
+regex_jc = ',,,(Broom|Cavendish|Conway|Egerton|Gell|Glossop|Headford|Victoria|Westminster|Wilkinson)'
+fn = sys.argv[1]
+fn = fn.replace('.csv', '')
 
-for line in fileinput.input():
-	if first:
-		print (line, end ='', file=fh_jillian)
-		print (line, end ='', file=fh_jon)
-		first =0
-		
-	if re.match(regex_jc, line):
-		print (line, end ='', file=fh_jillian)
-	else:
-		print (line, end ='', file=fh_jon)
+fh_jillian = open(fn + '_Jillian.csv', 'w')
+fh_jon = open(fn + '_Jon.csv', 'w')
+
+lines = fileinput.input()
+
+# Print header
+line1 = next(lines)
+print (line1, end='', file=fh_jillian)
+print (line1, end='', file=fh_jon)
+
+for line in lines:
+    if re.match(regex_jc, line):
+        print (line, end='', file=fh_jillian)
+    else:
+        print (line, end='', file=fh_jon)
--- SRGP/street_split.py
+++ SRGP/street_split.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
-#Split GH
-#Julian Briggs
-#4-may-2015
-#~ Broom Green
-#~ Broom Walk
-#~ Broomspring Close
-#~ Cavendish Court
-#~ Conway St
-#~ Egerton Close
-#~ Egerton Walk
-#~ Gell St
-#~ Glossop Rd
-#~ Headford Green
-#~ Headford Grove
-#~ Headford Mews
-#~ Victoria St
-#~ Wilkinson St
-#~ Westminster Chambers
+# Split GH
+# Julian Briggs
+# 4-may-2015
+# ~ Broom Green
+# ~ Broom Walk
+# ~ Broomspring Close
+# ~ Cavendish Court
+# ~ Conway St
+# ~ Egerton Close
+# ~ Egerton Walk
+# ~ Gell St
+# ~ Glossop Rd
+# ~ Headford Green
+# ~ Headford Grove
+# ~ Headford Mews
+# ~ Victoria St
+# ~ Wilkinson St
+# ~ Westminster Chambers
 
 from __future__ import print_function
+
 import fileinput
-import sys
 import re
+import sys
 
-regex_streets=',,,([\w\s]+),'
-fn=sys.argv[1]
-fn=fn.replace('.csv','')
 
-first = 1
-fh = open(fn +'_street_split.csv', 'w')
+regex_streets = ',,,([\w\s]+),'
+fn = sys.argv[1]
+fn = fn.replace('.csv', '_street_split.csv')
 
-last_street=''
-for line in fileinput.input():
-	if first:
-		print (line, end ='', file=fh)
-		first =0
-	else:	
-		m = re.match(regex_streets, line)
-		if m and m.group(0) != last_street:
-			print ('', file=fh)
-			last_street = m.group(0)
-		print (line, end ='', file=fh)
+fh = open(fn, 'w')
+
+lines = fileinput.input()
+
+# Print header
+line1 = next(lines)
+print (line1, end='', file=fh)
+
+last_street = ''
+for line in lines:
+    m = re.match(regex_streets, line)
+    if m and m.group(0) != last_street:
+        print ('', file=fh)
+        last_street = m.group(0)
+    print (line, end='', file=fh)
