ab634ebb8d82d8a0d9ea636083d718d6e4893af6
Julian Briggs <Julian.Briggs@sheffield.ac.uk>
2015-05-05 17:52:35 +0100

    made pd_split reference remote dir

--- SRGP/pd_split.sh
+++ SRGP/pd_split.sh
@@ -3,5 +3,7 @@
 #Julian Briggs
 #3-may-2015
 
-for pd in GA GB GC GD GE GF GG GH GI; do egrep ",PD_ENO,|,$pd" $1 > central_ward_pds/CentralWard${pd}_1_2.csv; done
+cd $(dirname $1)/central_ward_pds
+
+for pd in GA GB GC GD GE GF GG GH GI; do egrep ",PD_ENO,|,$pd" $1 > CentralWard${pd}_1_2.csv; done
 
