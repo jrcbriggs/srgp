73d0f140b073483de2aa95753267cd16b72548b3
Julian Briggs <Julian.Briggs@sheffield.ac.uk>
2015-12-23 16:36:06 +0000

    filenames in csvlink.sh

--- SRGP/csvlink.sh
+++ SRGP/csvlink.sh
@@ -11,12 +11,14 @@ datadir=~/SRGP/register/2015_16/CentralConstituency
 field_names='prefix first_name middle_name last_name suffix registered_zip registered_address1 registered_address2 registered_address3'
 
 register_old=CentralConstituencyRegister2015-04-20NB.csv
-register_new=CentralConstituencyWardsRegister2015-12-01NB.csv
-register_linked=CentralConstituencyWardsRegisterLinked2015-12-01.csv
-training_file=training_file.json
+register_new=CentralConstituencyWardRegisters2015-12-01NB.csv
+#register_linked=CentralConstituencyWardRegistersLinked2015-12-01.csv
+register_linked=CentralConstituencyWardRegistersUNLinked2015-12-01.csv
+#training_file=training.json
 
 cd $datadir
 #rm -f $training_file
-perl -pi.bak -e 's/,,/,_,/g;' $register_old $register_new
-perl -pi.bak -e 's/,,/,_,/g;' $register_old $register_new #do twice to replace overlapping ,,,
-csvlink --inner_join --field_names $field_names --output_file $register_linked $register_old $register_new
+#perl -pi.bak -e 's/,,/,_,/g;' $register_old $register_new
+#perl -pi.bak -e 's/,,/,_,/g;' $register_old $register_new #do twice to replace overlapping ,,,
+#csvlink --inner_join --field_names $field_names --output_file $register_linked $register_old $register_new
+csvlink --field_names $field_names --output_file $register_linked --skip_training $register_old $register_new
